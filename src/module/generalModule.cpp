953ac021bc04e3ef89071c1c3586dfc656c6633a
Sinter Wong <sintercver@163.com>
2023-03-14 17:36:39 +0800

    init general module

--- src/module/generalModule.cpp
+++ src/module/generalModule.cpp
@@ -10,12 +10,14 @@
  */
 
 #include "generalModule.h"
-#include "pipeline.hpp"
 #include <cstddef>
 #include <cstdlib>
 #include <experimental/filesystem>
-#include <sys/stat.h>
-#include <unistd.h>
+
+#include "infer/preprocess.hpp"
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 
 namespace module {
 
@@ -32,7 +34,42 @@ void GeneralModule::forward(std::vector<forwardMessage> &message) {
       return;
     }
 
+    FrameBuf frameBufMessage = ptr->pool->read(buf.key);
+    auto image =
+        std::any_cast<std::shared_ptr<cv::Mat>>(frameBufMessage.read("Mat"));
+
+    cv::Rect region = cv::Rect{config->region[0][0], config->region[0][1],
+                               config->region[1][0], config->region[1][1]};
+
+    cv::Mat inferImage;
+    if (region.area() > 1) {
+
+      if (!infer::utils::cropImage(*image, inferImage, region, buf.frameType)) {
+        FLOWENGINE_LOGGER_ERROR("cropImage is failed, rect is {},{},{},{}, "
+                                "but the video resolution is {}x{}",
+                                region.x, region.y, region.width, region.height,
+                                image->cols, image->rows);
+        return;
+      } else {
+        inferImage = *image; // 需不需要clone再说吧
+      }
+    }
+
     // 此处开始逻辑编写
+    auto &apipes = config->algoPipelines;
+    // 按顺序执行每一个算法，后面的算法基于前面的算法
+    for (auto const &ap : apipes) {
+      std::vector<common::RetBox> regions{
+          {name, {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}}};
+      InferParams params{
+          name,
+          buf.frameType,
+          0.5,
+          regions,
+          {inferImage.cols, inferImage.rows, inferImage.channels()}};
+      InferResult ret;
+      std::string aname = ap.first;
+    }
   }
 }
 
