6fbb56f0ac562d4d0fdb518023b38595948f3b24
Sinter Wong <sintercver@163.com>
2023-05-04 11:25:00 +0800

    add test_vio2encoder.cpp

--- scripts/x3_deploy.sh
+++ scripts/x3_deploy.sh
@@ -7,8 +7,7 @@ scp -P 22 -r build/aarch64/bin \
 
 scp -P 9205 -r build/aarch64/bin \
                build/aarch64/lib \
-               conf \
-               tests/data/data \
+               conf/app \
                tests/data/models \
                root@114.242.23.39:/root/workspace/softwares/flowengine
 
