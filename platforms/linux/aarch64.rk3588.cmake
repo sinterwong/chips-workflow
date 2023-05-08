9638bc32d7de36cc880c3d85121d250256fadc6b
Sinter Wong <sintercver@163.com>
2023-05-08 10:32:38 +0000

    init rockchip

---
+++ platforms/linux/aarch64.rk3588.cmake
@@ -0,0 +1,37 @@
+MESSAGE(STATUS "Configure Cross Compiler")
+
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+SET(TARGET_OS linux)
+SET(TARGET_ARCH aarch64)
+SET(TARGET_HARDWARE rockchip)
+ADD_DEFINITIONS(-DTARGET_PLATFORM=2)
+
+# set ${CMAKE_C_FLAGS} and ${CMAKE_CXX_FLAGS}flag for cross-compiled process
+SET(CROSS_COMPILATION_ARM rockchip)
+SET(CROSS_COMPILATION_ARCHITECTURE aarch64)
+
+SET(CMAKE_C_COMPILER       ${TOOLCHAIN_ROOTDIR}/aarch64-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER     ${TOOLCHAIN_ROOTDIR}/aarch64-linux-gnu-g++)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -Wl,--allow-shlib-undefined")
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -march=armv8-a -mcpu=cortex-a53")
+set(CMAKE_CXX_FLAGS_DEBUG " -Wall -Werror -g -O0 ")
+set(CMAKE_C_FLAGS_DEBUG " -Wall -Werror -g -O0 ")
+set(CMAKE_CXX_FLAGS_RELEASE " -Wall -Werror -O3 ")
+set(CMAKE_C_FLAGS_RELEASE " -Wall -Werror -O3 ")
+
+# set searching rules for cross-compiler
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET(CMAKE_SKIP_BUILD_RPATH TRUE)
+SET(CMAKE_SKIP_RPATH TRUE)
+
+# set g++ param
+# -fopenmp link libgomp
+# SET(CMAKE_CXX_FLAGS "-std=c++17 -march=armv8-a -mfloat-abi=softfp -mfpu=neon-vfpv4 \
+#     -ffunction-sections \
+#     -fdata-sections -O2 -fstack-protector-strong -lm -ldl -lstdc++\
+#     ${CMAKE_CXX_FLAGS}")
---
+++ src/infer/rockchip/dnn_inference.hpp
@@ -0,0 +1,69 @@
+/**
+ * @file dnn_inference.hpp
+ * @author Sinter Wong (sintercver@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-08
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef __RKNN_INFERENCE_H_
+#define __RKNN_INFERENCE_H_
+
+#include "inference.h"
+#include "logger/logger.hpp"
+
+
+#include <opencv2/core/mat.hpp>
+#include <memory>
+
+namespace infer {
+namespace dnn {
+class AlgoInference : public Inference {
+public:
+  //!
+  //! \brief construction
+  //!
+  AlgoInference(const AlgoBase &_param) : mParams(_param) {}
+
+  //!
+  //! \brief destruction
+  //!
+  ~AlgoInference() { terminate(); }
+  //!
+  //! \brief initialize the network
+  //!
+  virtual bool initialize() override;
+
+  //!
+  //! \brief Runs the inference engine
+  //!
+  virtual bool infer(FrameInfo &, void **) override;
+
+  //!
+  //! \brief ProcessInput that the input is correct for infer
+  //!
+  bool processInput(void *) override;
+
+  //!
+  //! \brief Outside can get model information after model initialize
+  //!
+  virtual void getModelInfo(ModelInfo &) const override;
+
+private:
+  //!
+  //! \brief Release the resource
+  //!
+  int terminate() {
+    // len代表单次推理的输出数量
+    return 0;
+  }
+
+private:
+  //!< The parameters for the sample.
+  AlgoBase mParams;
+};
+} // namespace x3
+} // namespace infer
+#endif
