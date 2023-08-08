3189af850e7f4e9bf0769d19a18d2fdc73d2c22a
sintercver <sintercver@gmail.com>
2023-08-08 17:11:43 +0800

    add parallel rgb2nv12 to infer

--- tests/infer/test_color_convert.cpp
+++ tests/infer/test_color_convert.cpp
@@ -17,53 +17,6 @@ const auto initLogger = []() -> decltype(auto) {
 DEFINE_string(img_path, "", "Specify image path.");
 DEFINE_int32(times, 10, "Specify test times.");
 
-void YU122NV12(cv::Mat const &input, cv::Mat &output) {
-  int y_rows = input.rows * 2 / 3;
-  cv::Mat y = input.rowRange(0, y_rows).colRange(0, input.cols);
-  cv::Mat uv = input.rowRange(y_rows, input.rows).colRange(0, input.cols);
-  output.create(cv::Size(input.cols, input.rows), CV_8UC1);
-
-  y.copyTo(output.rowRange(0, y.rows).colRange(0, y.cols));
-
-  cv::Mat uvInterleaved =
-      output.rowRange(y_rows, input.rows).colRange(0, uv.cols);
-
-  cv::Mat u = uv.rowRange(0, uv.rows / 2).colRange(0, uv.cols / 2);
-  cv::Mat v = uv.rowRange(uv.rows / 2, uv.rows).colRange(0, uv.cols / 2);
-
-  // Use cv::parallel_for_ to parallelize the filling of uvInterleaved
-  cv::parallel_for_(cv::Range(0, uv.rows / 2), [&](const cv::Range &range) {
-    for (int row = range.start; row < range.end; ++row) {
-      uchar *uvInterleavedRow = uvInterleaved.ptr<uchar>(2 * row);
-      uchar *uRow = u.ptr<uchar>(row);
-      uchar *vRow = v.ptr<uchar>(row);
-
-      for (int col = 0; col < uv.cols / 2; ++col) {
-        uvInterleavedRow[2 * col] = uRow[col];
-        uvInterleavedRow[2 * col + 1] = vRow[col];
-      }
-      uvInterleavedRow = uvInterleaved.ptr<uchar>(2 * row + 1);
-      for (int col = 0; col < uv.cols / 2; ++col) {
-        uvInterleavedRow[2 * col] = uRow[col];
-        uvInterleavedRow[2 * col + 1] = vRow[col];
-      }
-    }
-  });
-}
-
-void RGB2NV12_parallel(cv::Mat const &input, cv::Mat &output) {
-  cv::Rect rect{0, 0, input.cols, input.rows};
-  if (rect.width % 2 != 0)
-    rect.width -= 1;
-  if (rect.height % 2 != 0) {
-    rect.height -= 1;
-  }
-  cv::Mat in_temp = input(rect);
-  cv::Mat temp;
-  cv::cvtColor(in_temp, temp, cv::COLOR_RGB2YUV_I420);
-  YU122NV12(temp, output);
-}
-
 int main(int argc, char **argv) {
 
   gflags::SetUsageMessage("some usage message");
@@ -80,16 +33,17 @@ int main(int argc, char **argv) {
   long long time_parallel = 0;
   for (int i = 0; i < FLAGS_times; ++i) {
     time_parallel += utils::measureTime(
-        [&]() { RGB2NV12_parallel(image_rgb, image_nv12_a); });
+        [&]() { infer::utils::RGB2NV12(image_rgb, image_nv12_a, true); });
   }
   cv::imwrite("test_color_convert_nv12_a.jpg", image_nv12_a);
   FLOWENGINE_LOGGER_INFO("parallel total time: {} ms, avg time: {} ms",
                          static_cast<double>(time_parallel) / 1000,
-                         static_cast<double>(time_parallel) / 1000 / FLAGS_times);
+                         static_cast<double>(time_parallel) / 1000 /
+                             FLAGS_times);
 
   for (int i = 0; i < FLAGS_times; ++i) {
     time_ori += utils::measureTime(
-        [&]() { infer::utils::RGB2NV12(image_rgb, image_nv12_b); });
+        [&]() { infer::utils::RGB2NV12(image_rgb, image_nv12_b, false); });
   }
   cv::imwrite("test_color_convert_nv12_b.jpg", image_nv12_b);
   FLOWENGINE_LOGGER_INFO("original total time: {} ms, avg time: {} ms",
