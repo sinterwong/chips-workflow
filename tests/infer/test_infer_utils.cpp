21730545745422134e3cfbd898fd313b599247e5
Sinter Wong <sintercver@163.com>
2023-09-22 12:43:37 +0800

    optimized the rgb2nv11

--- tests/infer/test_infer_utils.cpp
+++ tests/infer/test_infer_utils.cpp
@@ -57,75 +57,19 @@ void test_color_convert() {
   FLOWENGINE_LOGGER_INFO("NV12 crop image cost time: {} ms",
                          static_cast<float>(nv12_crop_cost) / 1000);
 
+  // nv12 resize
   cv::Mat image_nv12_resized;
-  for (int i = 0; i < 10; i++) {
-    // // nv12 resize
-    // cv::Mat image_nv12_resized;
-    // auto test_nv12_resize = [&image_nv12_croped, &image_nv12_resized]() {
-    //   cv::Mat temp;
-    //   NV12toRGB(image_nv12_croped, temp);
-    //   std::array<int, 2> shape{640, 640};
-    //   resizeInput(temp, false, shape);
-    //   RGB2NV12(temp, image_nv12_resized);
-    // };
-    // auto nv12_resize_cost = measureTime(test_nv12_resize);
-    // FLOWENGINE_LOGGER_INFO("NV12 resize image cost time: {} ms",
-    //                        static_cast<float>(nv12_resize_cost) / 1000);
-
-    // nv12 resize
-    auto test_nv12_resize = [&image_nv12_croped, &image_nv12_resized]() {
-      std::array<int, 2> shape{640, 640};
-      int y_rows = image_nv12_croped.rows * 2 / 3;
-      cv::Mat y = image_nv12_croped.rowRange(0, y_rows).colRange(
-          0, image_nv12_croped.cols);
-      cv::Mat uv = image_nv12_croped.rowRange(y_rows, image_nv12_croped.rows)
-                       .colRange(0, image_nv12_croped.cols);
-
-      // 将UV分别重塑为两个矩阵
-      cv::Mat u = uv.rowRange(0, uv.rows / 2).colRange(0, uv.cols / 2);
-      cv::Mat v = uv.rowRange(uv.rows / 2, uv.rows).colRange(0, uv.cols / 2);
-
-      cv::Mat y_resized, u_resized, v_resized;
-      cv::resize(y, y_resized, cv::Size(shape.at(0), shape.at(1)));
-      cv::resize(u, u_resized, cv::Size(shape.at(0) / 2, shape.at(1)) / 2);
-      cv::resize(v, v_resized, cv::Size(shape.at(0) / 2, shape.at(1)) / 2);
-      // cv::imwrite("y_resized.jpg", y_resized);
-      // cv::imwrite("u_resized.jpg", u_resized);
-      // cv::imwrite("v_resized.jpg", v_resized);
-
-      image_nv12_resized.create(cv::Size(shape.at(0), shape.at(1) * 3 / 2),
-                                CV_8UC1);
-      y_resized.copyTo(image_nv12_resized.rowRange(0, y_resized.rows)
-                           .colRange(0, y_resized.cols));
-      // 将重采样后的U和V分量并入一个矩阵中
-      cv::Mat uvInterleaved =
-          image_nv12_resized.rowRange(y_resized.rows, image_nv12_resized.rows)
-              .colRange(0, image_nv12_resized.cols);
-
-      // 将U和V矩阵分别复制到UV通道中，每个U/V对应一个2x2的位置
-      for (int row = 0; row < uvInterleaved.rows / 2; ++row) {
-        uchar *uvInterleavedRow = uvInterleaved.ptr<uchar>(2 * row);
-        uchar *uRow = u_resized.ptr<uchar>(row);
-        uchar *vRow = v_resized.ptr<uchar>(row);
-
-        for (int col = 0; col < uvInterleaved.cols / 2; ++col) {
-          uvInterleavedRow[2 * col] = uRow[col];
-          uvInterleavedRow[2 * col + 1] = vRow[col];
-        }
-
-        uvInterleavedRow = uvInterleaved.ptr<uchar>(2 * row + 1);
-
-        for (int col = 0; col < uvInterleaved.cols / 2; ++col) {
-          uvInterleavedRow[2 * col] = uRow[col];
-          uvInterleavedRow[2 * col + 1] = vRow[col];
-        }
-      }
-    };
-    auto nv12_resize_cost = measureTime(test_nv12_resize);
-    FLOWENGINE_LOGGER_INFO("NV12 resize image cost time: {} ms",
-                           static_cast<float>(nv12_resize_cost) / 1000);
-  }
-
+  auto test_nv12_resize = [&image_nv12_croped, &image_nv12_resized]() {
+    cv::Mat temp;
+    NV12toRGB(image_nv12_croped, temp);
+    std::array<int, 2> shape{640, 640};
+    resizeInput(temp, false, shape);
+    RGB2NV12(temp, image_nv12_resized);
+  };
+  auto nv12_resize_cost = measureTime(test_nv12_resize);
+  FLOWENGINE_LOGGER_INFO("NV12 resize image cost time: {} ms",
+                         static_cast<float>(nv12_resize_cost) / 1000);
+  // draw the results
   cv::imwrite("rgb_image.jpg", image_rgb);
   cv::imwrite("nv12_image.jpg", image_nv12);
   cv::imwrite("nv12_croped_image.jpg", image_nv12_croped);
@@ -136,7 +80,7 @@ void test_color_convert() {
   cv::imwrite("bgr_resized_image.jpg", image_resized_bgr);
 }
 
-void test_new_crop() {
+void test_poly_crop() {
   cv::Mat image_bgr = cv::imread(FLAGS_image_path);
   FLOWENGINE_LOGGER_INFO("Image size: {}x{}", image_bgr.cols, image_bgr.rows);
   // bgr to rgb
@@ -146,18 +90,25 @@ void test_new_crop() {
   cv::Mat image_nv12, image_nv12_croped, image_nv12_croped_bgr;
   RGB2NV12(image_rgb, image_nv12);
 
-  common::Points2i points{Point2i{100, 100}, Point2i{300, 100},
-                          Point2i{350, 400}, Point2i{80, 450}};
+  common::Points2i points{Point2i{0, 0}, Point2i{400, 100},
+                          Point2i{500, 400}, Point2i{10, 30}};
 
   RetBox bbox{"hello", points};
 
-  cropImage(image_rgb, image_rgb_croped, bbox, common::ColorType::RGB888, 0.5);
+  cropImage(image_rgb, image_rgb_croped, bbox, common::ColorType::RGB888, 0);
   cv::imwrite("test_new_crop_rgb.jpg", image_rgb_croped);
 
-  cropImage(image_nv12, image_nv12_croped, bbox, common::ColorType::NV12, 0.5);
+  auto test_nv12_crop = [&image_nv12, &image_nv12_croped, &bbox]() {
+    cropImage(image_nv12, image_nv12_croped, bbox, common::ColorType::NV12, 0);
+  };
+  auto nv12_crop_cost = measureTime(test_nv12_crop);
+  FLOWENGINE_LOGGER_INFO("NV12 crop image cost time: {} ms",
+                         static_cast<float>(nv12_crop_cost) / 1000);
+
   cv::imwrite("test_new_crop_nv12.jpg", image_nv12_croped);
 
-  cv::cvtColor(image_nv12_croped, image_nv12_croped_bgr, cv::COLOR_YUV2BGR_NV12);
+  cv::cvtColor(image_nv12_croped, image_nv12_croped_bgr,
+               cv::COLOR_YUV2BGR_NV12);
   cv::imwrite("test_new_crop_nv12_bgr.jpg", image_nv12_croped_bgr);
 }
 
@@ -166,9 +117,9 @@ int main(int argc, char **argv) {
   gflags::SetVersionString("1.0.0");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  // test_color_convert();
+  test_color_convert();
 
-  test_new_crop();
+  test_poly_crop();
 
   return 0;
 }
