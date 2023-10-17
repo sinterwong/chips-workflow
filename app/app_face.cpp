d645a0c6b9d783c4031119216821fbe3719232f2
Sinter Wong <sintercver@163.com>
2023-10-17 10:27:36 +0800

    pause to fix a bug

--- app/app_face.cpp
+++ app/app_face.cpp
@@ -1,10 +1,16 @@
 /**
  * @file app_face.cpp
  * @author Sinter Wong (sintercver@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-10-13
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
+
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
