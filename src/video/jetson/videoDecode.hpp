1873c02b45a80b49ca2f60da7dc7ce8656fd7b40
sintercver <sintercver@gmail.com>
2023-12-28 10:29:36 +0800

    sync jetson

--- src/video/jetson/videoDecode.hpp
+++ src/video/jetson/videoDecode.hpp
@@ -16,6 +16,7 @@
 #include "videoSource.h"
 #include <memory>
 #include <opencv2/core/mat.hpp>
+#include <shared_mutex>
 #include <thread>
 
 #include "vdecoder.hpp"
@@ -24,8 +25,7 @@ namespace video {
 
 class VideoDecode : private VDecoder {
 private:
-  std::string uri; // 流地址
-  int w, h;        // 流分辨率
+  std::shared_mutex stream_m;
   std::unique_ptr<videoSource> stream = nullptr;
   std::unique_ptr<joining_thread> consumer; // 消费者
   std::mutex frame_m;
@@ -34,24 +34,22 @@ private:
 
   void consumeFrame();
 
-  void fillOptionByCV(std::string &url, videoOptions &options);
+  void fillOptionByCV(std::string const &url, videoOptions &options);
 
 public:
   bool init() override;
 
-  bool run() override;
-
-  bool start(std::string const &) override;
+  bool start(std::string const &, int w = 0, int h = 0) override;
 
   bool stop() override;
 
-  inline bool isRunning() override { return stream && stream->IsStreaming(); }
+  inline bool isRunning() override {
+    std::shared_lock lk{stream_m};
+    return stream && stream->IsStreaming();
+  }
 
-  inline std::string getUri() override {
-    return uri;
-  } 
-  
   inline int getHeight() override {
+    std::shared_lock lk{stream_m};
     if (isRunning()) {
       return stream->GetHeight();
     }
@@ -59,6 +57,7 @@ public:
   }
 
   inline int getWidth() override {
+    std::shared_lock lk{stream_m};
     if (isRunning()) {
       return stream->GetWidth();
     }
@@ -66,6 +65,7 @@ public:
   }
 
   inline int getRate() override {
+    std::shared_lock lk{stream_m};
     if (isRunning()) {
       return stream->GetFrameRate();
     }
@@ -75,13 +75,10 @@ public:
   std::shared_ptr<cv::Mat> getcvImage() override;
 
   inline common::ColorType getType() const noexcept override {
-    return common::ColorType::RGB888;
+    return common::getPlatformColorType();
   }
 
   explicit VideoDecode() {}
-
-  explicit VideoDecode(std::string const &uri_, int w_ = 1920, int h_ = 1080)
-      : uri(uri_), w(w_), h(h_) {}
 
   ~VideoDecode() noexcept {}
 };
